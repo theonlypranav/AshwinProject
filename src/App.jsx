bad76c3775093bff2b4c268b7fd7d78cb505b9a0
PraWater <praneel.maddula@gmail.com>
2024-06-27 11:14:53 +0530

    add: authentication

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,65 @@
+import { useState, useEffect } from "react";
 import Login from "./components/Login";
-import NavBar from './components/NavBar';
+import NavBar from "./components/NavBar";
+import { createClient } from "@supabase/supabase-js";
 
+const supabase = createClient(
+  "https://gffbqldvokpadhsmcrdj.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdmZmJxbGR2b2twYWRoc21jcmRqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTk0NjMxNDQsImV4cCI6MjAzNTAzOTE0NH0.6uSLWmq33rddRKhhnAY524n1n2GMW9sKocdQpj7yQF8",
+);
 
 function App() {
+  const [session, setSession] = useState(null);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  useEffect(() => {
+    const session = supabase.auth.session();
+    setSession(session?.user);
+    const { data: authListener } = supabase.auth.onAuthStateChange(
+      (event, session) => {
+        switch (event) {
+          case "SIGNED_IN":
+            setSession(session?.user);
+            break;
+          case "SIGNED_OUT":
+            setSession(null);
+            break;
+          default:
+        }
+      },
+    );
+    return () => {
+      authListener.unsubscribe();
+    };
+  }, []);
+
+  async function login() {
+    const { data, error } = await supabase.auth.signIn({
+      email: email,
+      password: password,
+    });
+    if (error) {
+      alert(error.message);
+    }
+  }
+
+  const logout = async () => {
+    await supabase.auth.signOut();
+  };
 
   return (
     <div>
-    <NavBar />
-    <Login />
+      <NavBar />
+      <Login
+        login={login}
+        logout={logout}
+        setEmail={setEmail}
+        setPassword={setPassword}
+        session={session}
+      />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,15 +1,12 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import Image from "../assets/image.png";
 import Logo from "../assets/image.png";
 import GoogleSvg from "../assets/image-copy.png";
 import { FaEye } from "react-icons/fa6";
 import { FaEyeSlash } from "react-icons/fa6";
 
-
-
-const Login = () => {
-  const [ showPassword, setShowPassword ] = useState(false);
-
+const Login = ({ login, logout, setEmail, setPassword, session }) => {
+  const [showPassword, setShowPassword] = useState(false);
 
   return (
     <div className="login-main">
@@ -18,45 +15,80 @@ const Login = () => {
       </div>
       <div className="login-right">
         <div className="login-right-container">
-          <div className="login-logo">
-            <img src={Logo} alt="" />
-          </div>
-          <div className="login-center">
-            <h3>ASHWIN Portal</h3>
-            <h2>Welcome back, ASHA worker!</h2>
-            <p>Please enter your login details</p>
-            <form>
-              <input type="email" placeholder="Email" />
-              <div className="pass-input-div">
-                <input type={showPassword ? "text" : "password"} placeholder="Password" />
-                {showPassword ? <FaEyeSlash onClick={() => {setShowPassword(!showPassword)}} /> : <FaEye onClick={() => {setShowPassword(!showPassword)}} />}
-                
+          {!session ? (
+            <>
+              <div className="login-logo">
+                <img src={Logo} alt="" />
+              </div>
+              <div className="login-center">
+                <h3>ASHWIN Portal</h3>
+                <h2>Welcome back, ASHA worker!</h2>
+                <p>Please enter your login details</p>
+                <form>
+                  <input
+                    type="email"
+                    placeholder="Email"
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                  <div className="pass-input-div">
+                    <input
+                      type={showPassword ? "text" : "password"}
+                      placeholder="Password"
+                      onChange={(e) => setPassword(e.target.value)}
+                    />
+                    {showPassword ? (
+                      <FaEyeSlash
+                        onClick={() => {
+                          setShowPassword(!showPassword);
+                        }}
+                      />
+                    ) : (
+                      <FaEye
+                        onClick={() => {
+                          setShowPassword(!showPassword);
+                        }}
+                      />
+                    )}
+                  </div>
+
+                  <div className="login-center-options">
+                    <div className="remember-div">
+                      <input type="checkbox" id="remember-checkbox" />
+                      <label htmlFor="remember-checkbox">
+                        Remember for 30 days
+                      </label>
+                    </div>
+                    <a href="#" className="forgot-pass-link">
+                      Forgot password?
+                    </a>
+                  </div>
+                  <div className="login-center-buttons">
+                    <button type="button" onClick={login}>
+                      Log In
+                    </button>
+                    <button type="button">
+                      <img src={GoogleSvg} alt="" />
+                      Log In with Government Credentials
+                    </button>
+                  </div>
+                </form>
               </div>
 
-              <div className="login-center-options">
-                <div className="remember-div">
-                  <input type="checkbox" id="remember-checkbox" />
-                  <label htmlFor="remember-checkbox">
-                    Remember for 30 days
-                  </label>
-                </div>
-                <a href="#" className="forgot-pass-link">
-                  Forgot password?
-                </a>
-              </div>
+              <p className="login-bottom-p">
+                Don't have an account? <a href="#">Sign Up</a>
+              </p>
+            </>
+          ) : (
+            <>
+              <h2>Welcome back, you are logged in as:</h2>
+              <h1>{session.email}</h1>
               <div className="login-center-buttons">
-                <button type="button">Log In</button>
-                <button type="button">
-                  <img src={GoogleSvg} alt="" />
-                  Log In with Government Credentials
+                <button type="button" onClick={logout}>
+                  Log Out
                 </button>
               </div>
-            </form>
-          </div>
-
-          <p className="login-bottom-p">
-            Don't have an account? <a href="#">Sign Up</a>
-          </p>
+            </>
+          )}
         </div>
       </div>
     </div>
