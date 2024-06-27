65f4d03c0fcf6fddd56675ff176e5ebd332494f8
theonlypranav <152270812+theonlypranav@users.noreply.github.com>
2024-06-27 09:56:36 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Login from "./components/Login";
+import NavBar from './components/NavBar';
+
+
+function App() {
+
+  return (
+    <div>
+    <NavBar />
+    <Login />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './MyComponent.css'; // CSS file for component styling
+
+function MyComponent() {
+  return (
+    <div className="container">
+      {/* Upper Navigation Bar */}
+      <div className="upper-nav">
+        <ul><b>
+          <li>Home</li>
+          <li>Ashwin Dawa Prapatra</li>
+          <li>Department Login</li>
+          <li>Notices</li>
+          </b>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default MyComponent;
